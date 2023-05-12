f340a7419491d8192e6a291897481b78a874ecb6
Remi <remi.chaieb@gmail.com>
2023-05-12 16:04:40 +0200

    funktion implementierung

--- src/main/java/com/example/projektsoftwarepraktikum/repository/BenutzerRepository.java
+++ src/main/java/com/example/projektsoftwarepraktikum/repository/BenutzerRepository.java
@@ -4,5 +4,5 @@ import com.example.projektsoftwarepraktikum.entity.Benutzer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BenutzerRepository extends JpaRepository<Benutzer, Integer> {
-
+    Benutzer findByUsername(String username);
 }
