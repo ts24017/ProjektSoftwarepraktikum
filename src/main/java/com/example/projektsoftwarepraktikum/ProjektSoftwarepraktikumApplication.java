a111bd282cbcc5977720fd8a43107780473f1de3
st177278 <nico.boeklen@uni-hohenheim.de>
2023-05-12 13:32:19 +0200

    Initial commit

---
+++ src/main/java/com/example/projektsoftwarepraktikum/ProjektSoftwarepraktikumApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektsoftwarepraktikum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektSoftwarepraktikumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektSoftwarepraktikumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektsoftwarepraktikum/ProjektSoftwarepraktikumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektsoftwarepraktikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektSoftwarepraktikumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
