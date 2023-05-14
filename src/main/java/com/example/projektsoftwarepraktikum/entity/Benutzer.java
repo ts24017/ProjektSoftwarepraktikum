6847a3b3b1045c598b844db7b1235ce59e2cc467
NicoBoeklen <nico.boeklen@uni-hohenheim.de>
2023-05-14 20:32:50 +0200

    Annotationen für DB

--- src/main/java/com/example/projektsoftwarepraktikum/entity/Benutzer.java
+++ src/main/java/com/example/projektsoftwarepraktikum/entity/Benutzer.java
@@ -1,33 +1,47 @@
 package com.example.projektsoftwarepraktikum.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+
 import java.util.Set;
 
 @Entity
+@Table(name = "Benutzer")
 public class Benutzer {
 
     @Id
     @GeneratedValue
+    @Column(name = "userID")
     private Integer userId;
 
+    @Column(name = "username")
     private String username;
 
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "enabled")
     private boolean enabled = true;
 
-
     @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "benutzer_rolle", joinColumns = @JoinColumn(name = "userID"), inverseJoinColumns = @JoinColumn(name = "rolleID"))
     private Set<Rolle> roles;
-
 
     public Benutzer() {
         // empty constructor for Hibernate
     }
+
+    ///////////////////////////////////////////////
+    // Getter & Setter
+    ///////////////////////////////////////////////
 
     public Integer getUserId() {
         return userId;
--- src/main/java/com/example/projektsoftwarepraktikum/entity/Rolle.java
+++ src/main/java/com/example/projektsoftwarepraktikum/entity/Rolle.java
@@ -1,16 +1,25 @@
 package com.example.projektsoftwarepraktikum.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Rolle")
 public class Rolle {
 
     @Id
     @GeneratedValue
+    @Column(name = "rolleID")
     private Integer id;
 
+    @Column(name = "rolename")
     private String rolename;
 
     public Rolle() {
@@ -20,6 +29,10 @@ public class Rolle {
     public Rolle(String rolename) {
         this.rolename = rolename;
     }
+
+    ///////////////////////////////////////////////
+    // Getter & Setter
+    ///////////////////////////////////////////////
 
     public Integer getId() {
         return id;
